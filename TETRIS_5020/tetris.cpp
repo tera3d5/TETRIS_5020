b3d1dd984d30b10d7a9cd1a4066f6f102ca94d2a
tera3d5 <tera.yuu1998@gmail.com>
2018-12-10 21:54:45 +0900

    分岐どうしよう

--- TETRIS_5020/tetris.cpp
+++ TETRIS_5020/tetris.cpp
@@ -12,7 +12,7 @@ Tetris::Tetris()
     
 }
 
-enum piece_color
+enum 
 {
     water,
     orange,
@@ -23,6 +23,8 @@ enum piece_color
     purple,
     black
 };
+int piece_color = water;
+
 
 
 bool Tetris::init()
@@ -61,69 +63,76 @@ void Tetris::draw()
     rect_view.right = 1280;
     rect_view.bottom = 720;
 
-    switch( piece_color )
-    {
-    default:RECT rect_piece;
-        rect_piece.top = 957;
-        rect_piece.left = 688 + ((piece_color) * 25);
-        rect_piece.right = 713;
-        rect_piece.bottom = 982;
-        break;
-    }
+	switch (piece_color)
+	{
+	case water:
+		RECT rect_piece_water;
+		rect_piece_water.top = 957;
+		rect_piece_water.left = 688;
+		rect_piece_water.right = 713;
+		rect_piece_water.bottom = 982;
+		break;
+
+	case orange:
+		RECT rect_piece_orange;
+		rect_piece_orange.top = 957;
+		rect_piece_orange.left = 713;
+		rect_piece_orange.right = 738;
+		rect_piece_orange.bottom = 982;
+		break;
+
+	case green:
+		RECT rect_piece_green;
+		rect_piece_green.top = 957;
+		rect_piece_green.left = 738;
+		rect_piece_green.right = 763;
+		rect_piece_green.bottom = 982;
+		break;
+
+	case red:
+		RECT rect_piece_red;
+		rect_piece_red.top = 957;
+		rect_piece_red.left = 763;
+		rect_piece_red.right = 788;
+		rect_piece_red.bottom = 982;
+		break;
+
+	case blue:
+		RECT rect_piece_blue;
+		rect_piece_blue.top = 957;
+		rect_piece_blue.left = 788;
+		rect_piece_blue.right = 813;
+		rect_piece_blue.bottom = 982;
+		break;
+
+	case brown:
+		RECT rect_piece_brown;
+		rect_piece_brown.top = 957;
+		rect_piece_brown.left = 813;
+		rect_piece_brown.right = 838;
+		rect_piece_brown.bottom = 982;
+		break;
+
+	case purple:
+		RECT rect_piece_purple;
+		rect_piece_purple.top = 957;
+		rect_piece_purple.left = 838;
+		rect_piece_purple.right = 863;
+		rect_piece_purple.bottom = 982;
+		break;
+
+	case black:
+		RECT rect_piece_black;
+		rect_piece_black.top = 957;
+		rect_piece_black.left = 863;
+		rect_piece_black.right = 888;
+		rect_piece_black.bottom = 982;
+		break;
+	}
     
-    
-    RECT rect_piece_water;
-    rect_piece_water.top = 957;
-    rect_piece_water.left = 688;
-    rect_piece_water.right = 713;
-    rect_piece_water.bottom = 982;
-
-    RECT rect_piece_orange;
-    rect_piece_orange.top = 957;
-    rect_piece_orange.left = 713;
-    rect_piece_orange.right = 738;
-    rect_piece_orange.bottom = 982;
-
-    RECT rect_piece_green;
-    rect_piece_green.top = 957;
-    rect_piece_green.left = 738;
-    rect_piece_green.right = 763;
-    rect_piece_green.bottom = 982;
-
-    RECT rect_piece_red;
-    rect_piece_red.top = 957;
-    rect_piece_red.left = 763;
-    rect_piece_red.right = 788;
-    rect_piece_red.bottom = 982;
-
-    RECT rect_piece_blue;
-    rect_piece_blue.top = 957;
-    rect_piece_blue.left = 788;
-    rect_piece_blue.right = 813;
-    rect_piece_blue.bottom = 982;
-
-    RECT rect_piece_brown;
-    rect_piece_brown.top = 957;
-    rect_piece_brown.left = 813;
-    rect_piece_brown.right = 838;
-    rect_piece_brown.bottom = 982;
-
-    RECT rect_piece_purple;
-    rect_piece_purple.top = 957;
-    rect_piece_purple.left = 838;
-    rect_piece_purple.right = 863;
-    rect_piece_purple.bottom = 982;
-
-    RECT rect_piece_black;
-    rect_piece_black.top = 957;
-    rect_piece_black.left = 863;
-    rect_piece_black.right = 888;
-    rect_piece_black.bottom = 982;
-
-
 
     Sprite::draw( texture_, Vector2( 0.0F, 0.0F ), &rect_view );
-    Sprite::draw( texture_, Vector2( 0.0F, 0.0F ), &rect_piece_green );
+    Sprite::draw( texture_, Vector2( 0.0F, 0.0F ), piece_color );
 
 }
 
